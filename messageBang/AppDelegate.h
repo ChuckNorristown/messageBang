7fb3ab3cf273184df1f66187122e34b8b786c6d2
Sean Shields <sean@dmgctrl.com>
2013-10-15 17:32:53 -0400

    created .gitignore

---
+++ messageBang/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  messageBang
+//
+//  Created by Sean Shields on 10/15/13.
+//  Copyright (c) 2013 Sean Shields. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
