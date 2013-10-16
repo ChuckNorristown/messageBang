5db1bee2ede4ed43ce0226fa0964c7cec728fe4a
Sean Shields <sean@dmgctrl.com>
2013-10-16 09:24:44 -0400

    added nav icons & nav controls

---
+++ InboxViewController.h
@@ -0,0 +1,13 @@
+//
+//  InboxViewController.h
+//  messageBang
+//
+//  Created by Sean Shields on 10/16/13.
+//  Copyright (c) 2013 Sean Shields. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InboxViewController : UITableViewController
+
+@end
---
+++ InboxViewController.m
@@ -0,0 +1,67 @@
+//
+//  InboxViewController.m
+//  messageBang
+//
+//  Created by Sean Shields on 10/16/13.
+//  Copyright (c) 2013 Sean Shields. All rights reserved.
+//
+
+#import "InboxViewController.h"
+
+@interface InboxViewController ()
+
+@end
+
+@implementation InboxViewController
+
+
+
+- (void)viewDidLoad
+{
+    
+    [super viewDidLoad];
+    
+    [self performSegueWithIdentifier:@"showLogin" sender:self];
+    
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Navigation logic may go here. Create and push another view controller.
+    /*
+     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+     // ...
+     // Pass the selected object to the new view controller.
+     [self.navigationController pushViewController:detailViewController animated:YES];
+     */
+}
+
+@end
