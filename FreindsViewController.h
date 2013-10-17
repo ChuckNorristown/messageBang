db65ac6af5d282ed7de94ac783db291fa747bdbe
Sean Shields <sean@dmgctrl.com>
2013-10-17 10:44:07 -0400

    created friends list table

---
+++ FreindsViewController.h
@@ -0,0 +1,17 @@
+//
+//  FreindsViewController.h
+//  messageBang
+//
+//  Created by Sean Shields on 10/17/13.
+//  Copyright (c) 2013 Sean Shields. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
+
+@interface FreindsViewController : UITableViewController
+
+@property (nonatomic, strong) PFRelation *friendsRelation;
+@property (nonatomic, strong) NSArray *friends;
+
+@end
